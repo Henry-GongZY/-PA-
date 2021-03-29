eeb1c2044ffa0bcbd43bce914fca4162a88f64da
tracer-ics2017 <tracer@njuics.org>
2021-03-29 17:40:37 +0000

    >  compile 1811358 ics Linux e866d0481540 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 GNU/Linux  17:40:37 up  5:37,  1 user,  load average: 0.03, 0.04, 0.00

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -314,7 +314,16 @@ bool check_parentheses(int p,int q,bool *success){
 	}
         if(tokens[p].type != TK_LB || tokens[q].type != TK_RB)
                 return false;
-
+        flag = 1;
+        for(int i=p+1;i<q;i++) {
+                if(tokens[i].type == TK_LB)
+                                flag++;
+                else if(tokens[i].type == TK_RB)
+                                flag--;
+                if(flag <= 0)
+                                return false;
+                
+        }
 	return true;
 }
 
