258735745dceb21b720daf728d153705d7988b42
tracer-ics2017 <tracer@njuics.org>
2021-03-29 17:50:32 +0000

    >  compile 1811358 ics Linux e866d0481540 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 GNU/Linux  17:50:32 up  5:47,  1 user,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -299,13 +299,13 @@ int getreg(char *str, int type) {
 bool check_parentheses(int p,int q,bool *success){
         if(tokens[p].type != TK_LB || tokens[q].type != TK_RB)
                 return false;
-	int flag=0;
-	for(int i=p;i<=q;i++) {
+	int flag=1;
+	for(int i=p+1;i<q;i++) {
 		if(tokens[i].type == TK_LB)
 				flag++;
 		else if(tokens[i].type == TK_RB)
 				flag--;
-		if(flag < 0){ 
+		if(flag < 1){ 
                                 *success = false;
                                 return false;
                 }
