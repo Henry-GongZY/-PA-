8300513c23dad60e57ca059c4bb472b247ce172e
tracer-ics2017 <tracer@njuics.org>
2021-04-24 20:18:00 +0000

    >  compile 1811358 ics Linux ae29a7e5e78d 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 GNU/Linux  20:18:00 up  3:53,  1 user,  load average: 0.12, 0.05, 0.04

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -143,7 +143,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
    // esp <- esp - 4
    rtl_subi(&cpu.esp,&cpu.esp,4);
    // M[esp] <- src1
-   rtl_sm(&cpu.esp,src1,4);
+   rtl_sm(&cpu.esp,4,src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
