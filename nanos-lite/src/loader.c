0aa8c223d0b84ad03951694f1df5dff69e8b364e
tracer-ics2017 <tracer@njuics.org>
2021-05-24 04:51:14 +0000

    >  compile 1811358 ics Linux ae29a7e5e78d 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 GNU/Linux  04:51:13 up  1:11,  1 user,  load average: 0.17, 0.08, 0.07

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,13 @@
 #include "common.h"
+#include "memory.h"
+#include "fs.h"
 
+size_t get_ramdisk_size();
+void ramdisk_read(void *,off_t,size_t);
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  //pa3.1
+  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
