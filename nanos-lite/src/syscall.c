dc8e2e6ff970d1ebdfe0bf6b0ed02afd59ef3edb
tracer-ics2017 <tracer@njuics.org>
2021-05-26 10:12:34 +0000

    >  run 1811358 ics Linux ae29a7e5e78d 5.4.72-microsoft-standard-WSL2 #1 SMP Wed Oct 28 23:40:43 UTC 2020 x86_64 GNU/Linux  10:12:34 up 14:44,  1 user,  load average: 0.02, 0.04, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,14 +13,33 @@ static inline _RegSet* sys_exit(_RegSet *r){
   return NULL;
 }
 
+static inline _RegSet* sys_write(_RegSet *r){
+  
+  int fd = (int)SYSCALL_ARG2(r);
+  char *buf = (char *)SYSCALL_ARG3(r);
+  int len = (int)SYSCALL_ARG4(r);
+
+  if(fd == 1 || fd == 2){
+    for(int i = 0; i < len; i++) {
+        _putc(buf[i]);
+    }
+    SYSCALL_ARG1(r) = len;
+  }
+
+  return NULL;
+}
+
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
 
   switch (a[0]) {
+    //pa3.1
     case SYS_none: sys_none(r); break;
     case SYS_exit: sys_exit(r); break;
+    //pa3.2
+    case SYS_write: sys_write(r); break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
